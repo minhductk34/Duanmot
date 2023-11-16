3f7ac89bd86d3c9bc53a3268c727d866fa3b201b
minhductk34 <minhductk34@gmail.com>
2023-11-16 09:03:06 +0700

    Cmt code 16/11

--- Duanmot/Controllers/BaseController.php
+++ Duanmot/Controllers/BaseController.php
@@ -5,20 +5,42 @@ class BaseController
 {
     const VIEW_FOLDER_NAME = 'views';
     const MODEL_FOLDER_NAME = 'Models';
+
+    /**
+     * Hiển thị một view và truyền dữ liệu cho view đó.
+     *
+     * @param string $viewPath Đường dẫn tới view cần hiển thị.
+     * @param array $data Mảng chứa dữ liệu truyền cho view. Key của mảng sẽ được sử dụng như biến trong view.
+     * @return void
+     */
     protected function view($viewPath, array $data = [])
     {
+        // Extract các phần tử trong mảng $data thành các biến độc lập trong phạm vi hàm.
         foreach ($data as $key => $value) {
             $$key = $value;
         }
-       
 
-        require(self::VIEW_FOLDER_NAME . '/' . str_replace(".", "/", $viewPath) . '.php');
+        // Xây dựng đường dẫn tới file view dựa trên $viewPath và tổ chức thư mục.
+        $viewFile = self::VIEW_FOLDER_NAME . '/' . str_replace(".", "/", $viewPath) . '.php';
+
+        // Require file view để hiển thị nội dung.
+        require($viewFile);
     }
+
+    /**
+     * Tải một model vào controller.
+     *
+     * @param string $modelsPath Đường dẫn tới file model cần tải.
+     * @param array $data Mảng chứa dữ liệu truyền cho model. Key của mảng sẽ được sử dụng như biến trong model.
+     * @return void
+     */
     protected function loadModel($modelsPath, array $data = [])
     {
+        // Xây dựng đường dẫn tới file model dựa trên $modelsPath và tổ chức thư mục.
+        $modelFile = self::MODEL_FOLDER_NAME . '/' . $modelsPath . '.php';
 
-        // die($viewPath);
-        require_once(self::MODEL_FOLDER_NAME . '/' . $modelsPath . '.php');
+        // Require file model để tải model vào controller.
+        require_once($modelFile);
     }
 }
 
--- Duanmot/Models/CategoryModel.php
+++ Duanmot/Models/CategoryModel.php
@@ -1,24 +1,55 @@
 <?php 
- class CategoryModel extends BaseModel
- {
+// Lớp CategoryModel kế thừa từ BaseModel
+class CategoryModel extends BaseModel
+{
     const TABLE = 'category';
 
-    public function getAll($select = ['*'],$orderBy =[], $limit =10)
+    /**
+     * Lấy tất cả các bản ghi từ bảng category.
+     *
+     * @param array $select Các cột cần lấy (mặc định là tất cả).
+     * @param array $orderBy Cột sắp xếp và thứ tự (mặc định là không sắp xếp).
+     * @param int $limit Số lượng bản ghi trả về (mặc định là 10).
+     * @return array Mảng chứa các bản ghi.
+     */
+    public function getAll($select = ['*'], $orderBy = [], $limit = 10)
     {
-        return $this->all(self::TABLE,$select,$orderBy,$limit);
+        return $this->all(self::TABLE, $select, $orderBy, $limit);
     }
+
+    /**
+     * Lấy thông tin danh mục dựa trên ID.
+     *
+     * @param int $id ID của danh mục.
+     * @return array|null Mảng chứa thông tin danh mục hoặc null nếu không tìm thấy.
+     */
     public function get_by_id($id)
     {
-        return $this->find(self::TABLE,$id);
+        return $this->find(self::TABLE, $id);
     }
-    public function _update($id,$data)
+
+    /**
+     * Cập nhật thông tin danh mục.
+     *
+     * @param int $id ID của danh mục cần cập nhật.
+     * @param array $data Dữ liệu cần cập nhật.
+     * @return void
+     */
+    public function _update($id, $data)
     {
         $this->update(self::TABLE, $id, $data);
     }
+
+    /**
+     * Xóa danh mục.
+     *
+     * @param int $id ID của danh mục cần xóa.
+     * @return void
+     */
     public function _delete($id)
     {
-       $this->delete(self::TABLE,$id);
+        $this->delete(self::TABLE, $id);
     }
- }
+}
 
 ?>
